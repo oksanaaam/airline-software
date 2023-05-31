b9ed4988e08f24b7c9647572b3e79b8d8e448b3b
name <ksyusha0kubay@gmail.com>
2023-05-31 15:07:22 +0300

    Add models

--- airline/admin.py
+++ airline/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from airline.models import Airplane
+
+admin.site.register(Airplane)
--- airline/models.py
+++ airline/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Airplane(models.Model):
+    id = models.IntegerField(primary_key=True)
+    passenger_count = models.IntegerField()
